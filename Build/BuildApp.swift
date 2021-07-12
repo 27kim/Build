a2058b767989b42c88b03ae0f4742b9567f53d03
doohyeonkim <doohyeonkim@hyundai.com>
2021-07-12 11:57:11 +0900

    Initial Commit

---
+++ Build/BuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildApp.swift
+//  Build
+//
+//  Created by doohyeon on 2021/07/12.
+//
+
+import SwiftUI
+
+@main
+struct BuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Build/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Build
+//
+//  Created by doohyeon on 2021/07/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
